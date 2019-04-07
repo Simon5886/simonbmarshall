7507a36a57d9b70eae3bf2b054738e1c5c6755c2
Simon Marshall <simon5886@mac.com>
2019-04-07 19:50:21 +0100

    blog

--- simonbmarshall/settings.py
+++ simonbmarshall/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'simonbmarshall',
-        'USER': 'simonbm',
+        'USER': 'postgres',
         'PASSWORD': 'dhdS2e0$$',
         'HOST': 'localhost',
         'PORT': '',
